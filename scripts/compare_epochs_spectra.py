906f8ce0382ec8dcc0b92a9c39c13c9d1b34b0a4
Aayush Arya <tomriddle257@gmail.com>
2022-08-04 12:30:54 +0900

    also compare single pt profiles the same way

--- scripts/compare_epochs_spectra.py
+++ scripts/compare_epochs_spectra.py
@@ -7,7 +7,17 @@ plt.rc('font', **{'family':'serif'})#, 'serif':'Times'
 
 #h2o_ep1 = np.loadtxt('../data/ascii/epoch1/WHya_H2O.ascii')
 
-def compare_spectral_lines(ep1_line_path, ep2_line_path, line_name=''):
+def compare_spectral_lines(ep1_line_path, ep2_line_path, line_name='', axis='velocity'):
+    """
+    axis - 'velocity' or 'freq'
+    """
+
+    freq_axis = 'Velocity'
+    # whether to plot velocity or frequency on x-axis
+    if axis == 'freq':
+        freq_axis = 'Frequency'
+    
+    # load ascii
     ep1_data = pd.read_csv(ep1_line_path, header=1)
     ep2_data = pd.read_csv(ep2_line_path, header=1)
     fig, ax = plt.subplots(figsize=(6,6))
@@ -15,14 +25,15 @@ def compare_spectral_lines(ep1_line_path, ep2_line_path, line_name=''):
     velocities = np.linspace(-5, 85, num=90)
 
     #print("The velocities I'm using,", velocities)
-    
-    ax.fill_between(ep1_data['Velocity'], ep1_data['Flux Density'], color='dodgerblue',
+
+
+    ax.fill_between(ep1_data[freq_axis], ep1_data['Flux Density'], color='dodgerblue',
                 zorder=2, alpha=0.6, step='pre', label='Nov 2015')
-    ax.fill_between(ep2_data['Velocity'], ep2_data['Flux Density'],
+    ax.fill_between(ep2_data[freq_axis], ep2_data['Flux Density'],
                 alpha=0.6, step='pre', color='gold', label='Nov 2017')
 
-    #ax.step(ep1_data['Velocity'], ep1_data['Flux Density'], c='k', lw=0.25)
-    ax.step(ep2_data['Velocity'], ep2_data['Flux Density'], c='r', lw=0.25, alpha=0.4)
+    #ax.step(ep1_data[freq_axis], ep1_data['Flux Density'], c='k', lw=0.25)
+    ax.step(ep2_data[freq_axis], ep2_data['Flux Density'], c='r', lw=0.25, alpha=0.4)
 
     ax.minorticks_on()
 
@@ -33,7 +44,10 @@ def compare_spectral_lines(ep1_line_path, ep2_line_path, line_name=''):
     ax.set_ylabel('Flux Density', fontsize=14)
     plt.title(line_name, fontsize=14)
     plt.axhline(y=0, xmin=0, xmax=1, ls='--', color='gray')
-    plt.axvline(x=39.5, ymin=0, ymax=1, ls='--', lw=1, c='dimgray', zorder=-1)
+    if freq_axis == 'Velocity':
+        plt.axvline(x=39.5, ymin=0, ymax=1, ls='--', lw=1, c='dimgray', zorder=-1)
+    else:
+        print('To mark the hrv=39.5 km/s line, convert that into frequency first.')
 
     plt.legend(fontsize=13)
     plt.tight_layout()
@@ -63,8 +77,13 @@ if __name__ == '__main__':
                 'SiO $v=2$', 'SiO $v=7$',] #'SiO $v=1$', 'H$^{13}$CN', 'SO $3\Sigma=0$']
 
     for ep1_file, ep2_file, line_name in zip(ep1_files, ep2_files, line_names):
-        compare_spectral_lines(ep1_path + ep1_file, ep2_path + ep2_file, line_name)
+        compare_spectral_lines(ep1_path + ep1_file, ep2_path + ep2_file, line_name, axis='velocity')
         # and the absorption regions
         ep1_abs = ep1_file[:-6] + '_abs.ascii'
         ep2_abs = ep2_file[:-6] + '_abs.ascii'
-        compare_spectral_lines(ep1_path + ep1_abs, ep2_path + ep2_abs, line_name + ' absorption')
+        ep1_pt = ep1_file[:-6] + '_pt_profile.ascii'
+        ep2_pt = ep2_file[:-6] + '_pt_profile.ascii'
+        compare_spectral_lines(ep1_path + ep1_abs, ep2_path + ep2_abs, line_name + ' absorption',)
+                    #axis='freq')
+
+        compare_spectral_lines(ep1_path + ep1_pt, ep2_path + ep2_pt, line_name + ' single point profile')
